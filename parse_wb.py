ed1b40effd8a68855240524183df0682fa15a30f
a <otlalevtina@gmai.com>
2021-11-04 19:59:41 +0300

    New functionality (measuring execution time) Adding comments Adding gitignore

--- parse_wb.py
+++ parse_wb.py
@@ -1,59 +1,84 @@
+"""
+Web scraping popular books about the internet and technology from wildberries.ru
+Also saving data to xlsx file and measuring program's execution time
+"""
+
 import requests
 import pandas
-import openpyxl
-from bs4 import BeautifulSoup
+import time
 from pandas import ExcelWriter
+from bs4 import BeautifulSoup
 
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0"
 }
 books = []
+start_time = time.time()
 
 
-def get_request():
-    n = int(input(f"Сколько страниц нужно спарсить? "))
-    return n
-
-
-def get_html(url):
-    response = requests.get(url, headers=headers)
-    return response
-
-
-def get_content(html):
-    soup = BeautifulSoup(html, "lxml")
-    ev = soup.find_all("div", "product-card j-card-item")
-
-    for i in ev:
-        books.append({
-            'title': i.find("span", "goods-name").get_text(),
-            'price': i.find(class_="lower-price").get_text(strip=True).replace("\xa0₽", ""),
-            'image': "https:" + i.find("img", "j-thumbnail thumbnail")['src']
-        })
-
-
+# Logic of program
 def main():
-    ur = "https://www.wildberries.ru/catalog/knigi/nehudozhestvennaya-literatura/internet-i-tehnologii?sort=popular&page={}"
-    url = [ur.format(x) for x in range(1, get_request()+1)]
-    for u in url:
-        html = get_html(u)
+    n = int(input("Сколько страниц раздела 'Интернет и технологии' нужно спарсить?\n"))
+    url = "https://www.wildberries.ru/catalog/knigi/nehudozhestvennaya-literatura/internet-i-tehnologii?sort=popular&page="
+    for i in range(1, n + 1):
+        pages = str(i)
+        u = url + pages
+        html = get_request(u)
         get_content(html.text)
     save(books)
 
 
+# Send get request and get the page
+def get_request(url):
+    response = requests.get(url, headers=headers)
+    return response
+
+
+# Scraping data from site
+def get_content(html):
+    soup = BeautifulSoup(html, "lxml")
+    data = soup.find_all("div", class_="product-card j-card-item")
+
+    for d in data:
+        try:
+            books.append({
+                "title": d.find("span", class_="goods-name").get_text(),
+                "old-price": int(d.find(
+                    "del", class_="price-commission__old-price").get_text(strip=True).replace(
+                    "\xa0₽", "").replace("\xa0", "")),
+                "new-price": int(d.find(
+                    "span", class_="price-commission__current-price").get_text().replace(
+                    "\xa0₽", "").replace("\xa0", ""))
+            })
+        except AttributeError:
+            books.append({
+                "title": d.find("span", class_="goods-name").get_text(),
+                "old-price": int(d.find(
+                    "span", class_="lower-price").get_text().replace(
+                    "\xa0₽", "").replace("\xa0", ""))
+            })
+
+
+# Saving data to xlsx file and measuring program's execution time
 def save(info):
     dataframe = pandas.DataFrame(info)
-    newdataframe = dataframe.rename(columns = {
+    dataframe['difference'] = dataframe['old-price'] - dataframe['new-price']
+    newdataframe = dataframe.rename(columns={
         "title": "Название",
-        "price": "Цена",
-        "image": "Обложка"
+        "new-price": "Со скидкой",
+        "old-price": "Без скидки",
+        "difference": "Экономия"
     })
-    name = input("Введите имя файла: ")
+    name = input("Введите имя файла без указания формата: ")
     writer = ExcelWriter(f"{name}.xlsx")
-    newdataframe.to_excel(writer, f"name")
+    newdataframe.to_excel(writer, index=False)
     writer.save()
     print(f"Данные сохранены в файл '{name}.xlsx'")
+    print(f"Время работы программы: {int(time.time() - start_time) // 60} минут "
+          f"{int(time.time() - start_time) % 60} секунд")
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
+
