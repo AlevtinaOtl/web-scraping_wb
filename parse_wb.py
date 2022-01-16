7afd0c84b5f8f326b3bd59e9a49ea14b05a606b2
a <otlalevtina@gmai.com>
2022-01-17 00:35:05 +0300

    Async version of web scraping

--- parse_wb.py
+++ parse_wb.py
@@ -1,68 +1,71 @@
 """
-Web scraping popular books about the internet and technology from wildberries.ru
+Async web scraping popular books about the internet and technology from wildberries.ru
 Also saving data to xlsx file and measuring program's execution time
 """
 
-import requests
-import pandas
 import time
+import pandas
 from pandas import ExcelWriter
 from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0"
-}
 books = []
 start_time = time.time()
 
 
-# Logic of program
-def main():
-    n = int(input("Сколько страниц раздела 'Интернет и технологии' нужно спарсить?\n"))
-    url = "https://www.wildberries.ru/catalog/knigi/nehudozhestvennaya-literatura/internet-i-tehnologii?sort=popular&page="
-    for i in range(1, n + 1):
-        pages = str(i)
-        u = url + pages
-        html = get_request(u)
-        get_content(html.text)
-    save(books)
+# get async scraping task done
+async def get_page(session, page):
+    headers = {
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0"
+    }
+    url = f"https://www.wildberries.ru/catalog/knigi/nehudozhestvennaya-literatura/internet-i-tehnologii?sort=popular&page={page}"
+    async with session.get(url=url, headers=headers) as response:
+        response_text = await response.text()
+        soup = BeautifulSoup(response_text, "lxml")
+        data = soup.find_all("div", class_="product-card j-card-item")
+
+        for d in data:
+            try:
+                books.append({
+                    "title": d.find("span", class_="goods-name").get_text(),
+                    "old-price": int(d.find(
+                        "span", class_="price-old-block").get_text(strip=True).replace(
+                        "\xa0₽", "").replace("\xa0", "")),
+                    "new-price": int(d.find(
+                        "ins", class_="lower-price").get_text().replace(
+                        "\xa0₽", "").replace("\xa0", ""))
+                })
+            except AttributeError:
+                books.append({
+                    "title": d.find("span", class_="goods-name").get_text(),
+                    "old-price": int(d.find(
+                        "span", class_="lower-price").get_text().replace(
+                        "\xa0₽", "").replace("\xa0", ""))
+                })
 
 
-# Send get request and get the page
-def get_request(url):
-    response = requests.get(url, headers=headers)
-    return response
+# create tasks query
+async def collect_data():
+    async with aiohttp.ClientSession() as session:
+        pages = int(input("Сколько страниц раздела 'Интернет и технологии' нужно спарсить?\n"))
+
+        for page in range(1, pages + 1):
+            book = asyncio.create_task(get_page(session, page))
+            books.append(book)
+        await asyncio.gather(*books)
+    save()
 
 
-# Scraping data from site
-def get_content(html):
-    soup = BeautifulSoup(html, "lxml")
-    data = soup.find_all("div", class_="product-card j-card-item")
+# save information to xlsx file
+def save():
+    books_list = []
 
-    for d in data:
-        try:
-            books.append({
-                "title": d.find("span", class_="goods-name").get_text(),
-                "old-price": int(d.find(
-                    "del", class_="price-commission__old-price").get_text(strip=True).replace(
-                    "\xa0₽", "").replace("\xa0", "")),
-                "new-price": int(d.find(
-                    "span", class_="price-commission__current-price").get_text().replace(
-                    "\xa0₽", "").replace("\xa0", ""))
-            })
-        except AttributeError:
-            books.append({
-                "title": d.find("span", class_="goods-name").get_text(),
-                "old-price": int(d.find(
-                    "span", class_="lower-price").get_text().replace(
-                    "\xa0₽", "").replace("\xa0", ""))
-            })
-
-
-# Saving data to xlsx file and measuring program's execution time
-def save(info):
-    dataframe = pandas.DataFrame(info)
+    for book in books:
+        if type(book) == dict:
+            books_list.append(book)
+    dataframe = pandas.DataFrame(books_list)
     dataframe['difference'] = dataframe['old-price'] - dataframe['new-price']
     newdataframe = dataframe.rename(columns={
         "title": "Название",
@@ -75,10 +78,16 @@ def save(info):
     newdataframe.to_excel(writer, index=False)
     writer.save()
     print(f"Данные сохранены в файл '{name}.xlsx'")
+
+
+def main():
+    try:
+        asyncio.run(collect_data())
+    except BaseException:
+        print("Problem. Try to scrape less pages.")
     print(f"Время работы программы: {int(time.time() - start_time) // 60} минут "
           f"{int(time.time() - start_time) % 60} секунд")
 
 
 if __name__ == '__main__':
     main()
-
